ea85020afdb4c19598d46bb3633bed3862ef5f09
Claudia Jessica Fernandez Miranda <jessicaccfernandez@gmail.com>
2020-09-02 01:49:55 -0300

    Modificacion

--- app/src/main/java/com/example/tp01/BroadcastDinamico.java
+++ app/src/main/java/com/example/tp01/BroadcastDinamico.java
@@ -1,42 +1,36 @@
 package com.example.tp01;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+
 public class BroadcastDinamico extends BroadcastReceiver {
-    String estadoLlamando;
-    String estadoUSB;
-    String numero;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
         if (intent.getAction().equals("android.hardware.usb.action.USB_STATE")) {
             if (intent.getExtras().getBoolean("connected")) {
-                Toast.makeText(context, "USB: CONECTADO", Toast.LENGTH_SHORT).show();
-                estadoUSB = "conectado";
+
+                Intent llamado = new Intent(Intent.ACTION_CALL);
+                llamado.setData(Uri.parse("tel:" + 9111));
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+
+                    return;
+                }
+                Toast.makeText(context, "USB: CONECTADO, LLAMANDO", Toast.LENGTH_SHORT).show();
+                context.startActivity(llamado);
             } else {
-                Toast.makeText(context, "USB: DESCONECTADO", Toast.LENGTH_SHORT).show();
-                estadoUSB = "desconectado";
+                Toast.makeText(context, "USB: DESCONECTADO, NO LLAMA", Toast.LENGTH_SHORT).show();
             }
 
         }
-        if (intent.getAction().equals("tp1.broadcast.RECEIVED")) {
-            estadoLlamando="llamando";
-
-        }else estadoLlamando="nollamando";
-
-
-        if(estadoLlamando.equals("llamando") && estadoUSB.equals("conectado")){
-            numero = intent.getStringExtra("telefono");
-            if(numero.equals("9111")){
-                Toast.makeText(context, "Llamando al 9111, Usb Conectado", Toast.LENGTH_SHORT).show();
-            }
-
-        }
-
     }
 
 
--- app/src/main/java/com/example/tp01/MainActivity.java
+++ app/src/main/java/com/example/tp01/MainActivity.java
@@ -1,79 +1,44 @@
 package com.example.tp01;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Toast;
+
 
 
 public class MainActivity extends AppCompatActivity {
-    private EditText etNum;
-    private ImageButton btnLlamado;
     private IntentFilter filtroIntent;
-    private BroadcastDinamico dinamico = new BroadcastDinamico();
+    private BroadcastDinamico dinamico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        filtroIntent = new IntentFilter();
+        filtroIntent.addAction("android.hardware.usb.action.USB_STATE");
         if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M
                 && checkSelfPermission(Manifest.permission.CALL_PHONE)
                 !=PackageManager.PERMISSION_GRANTED){
             requestPermissions(new String[]{Manifest.permission.CALL_PHONE},1000);
         }
-
-        filtroIntent = new IntentFilter();
-        filtroIntent.addAction("tp1.broadcast.RECEIVED");
-        filtroIntent.addAction("android.hardware.usb.action.USB_STATE");
-        etNum = findViewById(R.id.etTelefono);
-        btnLlamado = findViewById(R.id.btnLlamar);
-
-        btnLlamado.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String numero = etNum.getText().toString();
-                if(numero.matches("[0-9]{3,8}")){
-                    Intent intent = new Intent(Intent.ACTION_CALL);
-                    intent.setData(Uri.parse("tel:" + numero));
-                    Intent llamado = new Intent("tp1.broadcast.RECEIVED");
-                    llamado.putExtra("telefono", numero);
-                    if (ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                        // TODO: Consider calling
-                        //    ActivityCompat#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for ActivityCompat#requestPermissions for more details.
-                        return;
-                    }
-                    sendBroadcast(llamado);
-                    startActivity(intent);
-                }
-                else Toast.makeText(getApplication(), "Ingresa numero, de 3 o mas caracteres", Toast.LENGTH_SHORT).show();
-
-            }
-        });
+        dinamico = new BroadcastDinamico();
         registerReceiver(dinamico, filtroIntent);
     }
 
+    // @Override
+    //protected void onResume() {
+    //    super.onResume();
+    //    dinamico = new BroadcastDinamico();
+    //    registerReceiver(dinamico, filtroIntent);
+    //}
 
-
-
-
-
-
+    //@Override
+    //protected void onPause() {
+    //    super.onPause();
+    //    unregisterReceiver(dinamico);
+    //}
 }
